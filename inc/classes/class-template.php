9d3fd21d9404466e1a5d83b2f8504d3caf1d6893
Shah Jalal <rjshahjalal132@gmail.com>
2024-10-26 01:39:25 +0600

    added program logs trait

--- inc/classes/class-template.php
+++ inc/classes/class-template.php
@@ -2,11 +2,13 @@
 
 namespace BULK_IMPORT\Inc;
 
+use BULK_IMPORT\Inc\Traits\Program_Logs;
 use BULK_IMPORT\Inc\Traits\Singleton;
 
 class Class_Template {
 
     use Singleton;
+    use Program_Logs;
 
     public function __construct() {
         $this->setup_hooks();
