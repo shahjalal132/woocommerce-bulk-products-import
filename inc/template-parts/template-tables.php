464e7e8d8210989b0ad41fb9a11e61f097b428fb
Muhammad Shah jalal <128796759+shahjalal132@users.noreply.github.com>
2024-11-05 21:09:54 +0600

    Update template-tables.php

--- inc/template-parts/template-tables.php
+++ inc/template-parts/template-tables.php
@@ -3,25 +3,25 @@
 $table_prefix = get_option( 'be-table-prefix' ) ?? '';
 ?>
 
-<div id="db-tables" class="common-shadow container mt-4">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
+<div id="db-tables" class="common-shadow mt-4">
+    <div class="row">
+        <div class="col-sm-12">
             <!-- Title for the Table Prefix Section -->
             <h4 class="text-center mb-4">
-                <?php esc_html_e( 'Custom Table Prefix', 'bulk-product-import' ); ?>
+                <?php esc_html_e( 'Custom Table Prefix', 'just-another-panel' ); ?>
             </h4>
 
             <!-- Form to set the table prefix -->
             <form action="" method="post">
-                <div class="row mb-3">
+                <div class="row align-items-center justify-content-between mb-3">
                     <!-- Label for Table Prefix -->
-                    <label class="col-sm-4 col-form-label text-end" for="table-prefix">
-                        <?php esc_html_e( 'Table Prefix', 'bulk-product-import' ); ?>
+                    <label class="col-sm-3 col-form-label" for="table-prefix">
+                        <?php esc_html_e( 'Table Prefix', 'just-another-panel' ); ?>
                     </label>
                     <!-- Input for Table Prefix -->
-                    <div class="col-sm-8">
+                    <div class="col-sm-9">
                         <input type="text" class="form-control" name="table-prefix" id="table-prefix"
-                               placeholder="<?php esc_attr_e( 'Enter Table Prefix', 'bulk-product-import' ); ?>"
+                               placeholder="<?php esc_attr_e( 'Enter Table Prefix', 'just-another-panel' ); ?>"
                                value="<?php echo esc_attr( $table_prefix ); ?>">
                     </div>
                 </div>
@@ -30,7 +30,7 @@ $table_prefix = get_option( 'be-table-prefix' ) ?? '';
                 <div class="row">
                     <div class="col text-start">
                         <input type="submit" class="btn btn-primary" id="save-table-prefix"
-                               value="<?php esc_attr_e( 'Save', 'bulk-product-import' ); ?>">
+                               value="<?php esc_attr_e( 'Save', 'just-another-panel' ); ?>">
                     </div>
                 </div>
             </form>
